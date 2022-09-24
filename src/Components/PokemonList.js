bd85b6d212cce9a55fd996d2999725700aa665b5
Ewelina <ewelina.kuzniewska@wp.pl>
2022-09-24 13:47:38 +0200

    message

--- src/Components/PokemonList.js
+++ src/Components/PokemonList.js
@@ -14,11 +14,10 @@ const S = {
     align-items: center;
   `,
   Card: styled.div`
- display: flex; 
- flex-wrap: wrap;
- justify-content: space-around;
-;
-  `
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+  `,
 }
 
 const PokemonList = () => {
@@ -26,10 +25,14 @@ const PokemonList = () => {
   const [nextURL, setNextURL] = useState()
   const [prevURL, setPrevURL] = useState()
   const [loading, setLoading] = useState(true)
+  const [url, setUrl] = useState(
+    'https://pokeapi.co/api/v2/pokemon?limit=15&offset=0'
+  )
+  const [inputText, setInputText] = useState(null)
 
   const fetchPokemons = async () => {
     try {
-      const res = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=15&offset=0')
+      const res = await axios.get(url)
 
       return res
     } catch (err) {
@@ -52,26 +55,30 @@ const PokemonList = () => {
     fetchPokemons()
   }, [])
 
+  useEffect(() => {
+  const filterPoke = data.data.results.filter((poke) => {
+    if(poke.name.toLowerCase().includes(inputText.toLoverCase()))
+      return <PokemonCard key={poke.name}/>
+    })}, [inputText])
+
   return (
     <div>
-     {status === 'loading' && <h2>Loading...</h2>}
+      {status === 'loading' && <h2>Loading...</h2>}
       {status === 'error' && <h2>Error!</h2>}
       {status === 'success' && (
         <div>
-       
+        <input onChange={(e) => setInputText(e.target.value)}/>{inputText}
           <S.Card>
             {data &&
               data.data.results.map((pokemon) => (
-            
                 <PokemonCard
-                  key={pokemon.url}
+                  key={pokemon.name}
                   name={pokemon.name}
                   url={pokemon.url}
                   pokemon={pokemon}
                 />
               ))}
           </S.Card>
-          
         </div>
       )}
       <button onClick={prevPage}>Previous</button>
